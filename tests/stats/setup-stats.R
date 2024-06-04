3c3b9c0ca62d613d6593600c31bb2735f2c4886a
Alan Haynes <aghaynes@users.noreply.github.com>
2024-06-04 09:48:14 +0200

    Update setup-stats.R

--- tests/stats/setup-stats.R
+++ tests/stats/setup-stats.R
@@ -1,5 +1,6 @@
 library(stats)
 library(testthat)
+local_edition(3) # use testthat 3rd edition syntax (waldo)
 withr::defer({
   # most of the time, we would want to detach packages, in this case we don't
   # detach(package:stats)
